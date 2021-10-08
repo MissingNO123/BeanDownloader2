41637fcfbfa2a0e3da87f26d4689281fd2b43103
Julia <MissingNO123@users.noreply.github.com>
2021-10-07 20:51:06 -0400

    Update YT Explode

--- BeanDownloader2/Properties/AssemblyInfo.cs
+++ BeanDownloader2/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BeanDownloader2")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright ©MissingNO123 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
